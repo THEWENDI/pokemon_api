db794a8423519c9f6e38f2983135f9bc918ff066
Wendi Li <issacbbb@hotmaill.com>
2022-03-15 20:46:15 -0700

    with bonus question

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,7 +2,28 @@ import React, {useState} from 'react'
 import axios from 'axios'
 
 const Pokemon = () => {
-    const[pokemons, setPokemons] = useState()
+    const[pokemons, setPokemons] = useState();
+    const[pokemonName, setPokemonName] = useState("");
+    const[pokemonChosen, setPokemonChosen] = useState(false);
+    const[spokemon, setsPokemon] = useState({
+        name: "",
+        species: "",
+        img: "",
+    });
+
+    const searchPokemon = () =>{
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            .then(response =>{
+                console.log(response.data)
+                setsPokemon({
+                    name: pokemonName,
+                    species: response.data.species.name,
+                    img: response.data.sprites.front_default,
+                });
+                setPokemonChosen(true);
+            })
+            .catch(err => console.log(err))
+    }
 
     const fetchPokemonAwait = async() => {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=807`)
@@ -41,6 +62,21 @@ const Pokemon = () => {
             <h1>Click the button!</h1>
 
         }
+        <div>
+        <h1>Pokemon</h1>
+        <input type="text" onChange={(e)=>setPokemonName(e.target.value)}/>
+        <button onClick={searchPokemon}>Search Pokemon</button>
+
+        {pokemonChosen?
+            <div>
+                <h1>{spokemon.name}</h1>
+                <img style={{height: '200px'}} src={spokemon.img}/>
+                <h3>Species: {spokemon.species}</h3>
+            </div>:
+            <h1>Search Pokemon</h1>
+
+        }
+        </div>
     </div>
 
   )
